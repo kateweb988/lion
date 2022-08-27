95b3bf278db98c2b3f63043454308ff40c3f506a
kate <katyavov2805@gmail.com>
2022-08-27 15:50:59 +0300

    pages

--- dev/js/all.js
+++ dev/js/all.js
@@ -1,5 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
 });
+//popup
+let popupBg = document.querySelector('.popup__bg');
+let popup = document.querySelector('.popup');
+let openPopupButtons = document.querySelectorAll('.open-popup');
+let closePopupButton = document.querySelector('.close-popup');
+
+openPopupButtons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        e.preventDefault();
+        popupBg.classList.add('active');
+        popup.classList.add('active');
+    })
+});
+
+closePopupButton.addEventListener('click',() => {
+    popupBg.classList.remove('active');
+    popup.classList.remove('active');
+});
+
+document.addEventListener('click', (e) => {
+    if(e.target === popupBg) {
+        popupBg.classList.remove('active');
+        popup.classList.remove('active');
+    }
+});
+//accordeon
 var accordeonButtons = document.getElementsByClassName("accordeon__button");
 
 //пишем событие при клике на кнопки - вызов функции toggle
@@ -36,3 +62,27 @@ function toggleItems() {
     }
 
 }
+let popupBg2 = document.querySelector('.popup__bg2');
+let popup2 = document.querySelector('.popup2');
+let openPopupButtons2 = document.querySelectorAll('.open-popup2');
+let closePopupButton2 = document.querySelector('.close-popup2');
+
+openPopupButtons2.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        e.preventDefault();
+        popupBg2.classList.add('active');
+        popup2.classList.add('active');
+    })
+});
+
+closePopupButton2.addEventListener('click',() => {
+    popupBg2.classList.remove('active');
+    popup2.classList.remove('active');
+});
+
+document.addEventListener('click', (e) => {
+    if(e.target === popupBg2) {
+        popupBg2.classList.remove('active');
+        popup2.classList.remove('active');
+    }
+});
