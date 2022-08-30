40f955d1aa1ecd4e4b389fa5c4f9d5a3e0804271
kate <katyavov2805@gmail.com>
2022-08-30 21:07:15 +0300

    fix

--- src/js/main.js
+++ src/js/main.js
@@ -1,30 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
 });
-//popup
-let popupBg = document.querySelector('.popup__bg');
-let popup = document.querySelector('.popup');
-let openPopupButtons = document.querySelectorAll('.open-popup');
-let closePopupButton = document.querySelector('.close-popup');
-
-openPopupButtons.forEach((button) => {
-    button.addEventListener('click', (e) => {
-        e.preventDefault();
-        popupBg.classList.add('active');
-        popup.classList.add('active');
-    })
-});
-
-closePopupButton.addEventListener('click',() => {
-    popupBg.classList.remove('active');
-    popup.classList.remove('active');
-});
-
-document.addEventListener('click', (e) => {
-    if(e.target === popupBg) {
-        popupBg.classList.remove('active');
-        popup.classList.remove('active');
-    }
-});
 //accordeon
 var accordeonButtons = document.getElementsByClassName("accordeon__button");
 
@@ -62,6 +37,31 @@ function toggleItems() {
     }
 
 }
+//popup
+let popupBg = document.querySelector('.popup__bg');
+let popup = document.querySelector('.popup');
+let openPopupButtons = document.querySelectorAll('.open-popup');
+let closePopupButton = document.querySelector('.close-popup');
+
+openPopupButtons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        e.preventDefault();
+        popupBg.classList.add('active');
+        popup.classList.add('active');
+    })
+});
+
+closePopupButton.addEventListener('click',() => {
+    popupBg.classList.remove('active');
+    popup.classList.remove('active');
+});
+
+document.addEventListener('click', (e) => {
+    if(e.target === popupBg) {
+        popupBg.classList.remove('active');
+        popup.classList.remove('active');
+    }
+});
 let popupBg2 = document.querySelector('.popup__bg2');
 let popup2 = document.querySelector('.popup2');
 let openPopupButtons2 = document.querySelectorAll('.open-popup2');
