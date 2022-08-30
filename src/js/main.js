d8f5b7f63275afd44d9cec2bc8a78c2cb5e8f6d9
kate <katyavov2805@gmail.com>
2022-08-30 20:46:04 +0300

    popups

--- src/js/main.js
+++ src/js/main.js
@@ -86,3 +86,78 @@ document.addEventListener('click', (e) => {
         popup2.classList.remove('active');
     }
 });
+
+let popupBg3 = document.querySelector('.popup__bg3');
+let popup3 = document.querySelector('.popup3');
+let openPopupButtons3 = document.querySelectorAll('.open-popup3');
+let closePopupButton3 = document.querySelector('.close-popup3');
+
+openPopupButtons3.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        e.preventDefault();
+        popupBg3.classList.add('active');
+        popup3.classList.add('active');
+    })
+});
+
+closePopupButton3.addEventListener('click',() => {
+    popupBg3.classList.remove('active');
+    popup3.classList.remove('active');
+});
+
+document.addEventListener('click', (e) => {
+    if(e.target === popupBg3) {
+        popupBg3.classList.remove('active');
+        popup3.classList.remove('active');
+    }
+});
+
+let popupBg4 = document.querySelector('.popup__bg4');
+let popup4 = document.querySelector('.popup4');
+let openPopupButtons4 = document.querySelectorAll('.open-popup4');
+let closePopupButton4 = document.querySelector('.close-popup4');
+
+openPopupButtons4.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        e.preventDefault();
+        popupBg4.classList.add('active');
+        popup4.classList.add('active');
+    })
+});
+
+closePopupButton4.addEventListener('click',() => {
+    popupBg4.classList.remove('active');
+    popup4.classList.remove('active');
+});
+
+document.addEventListener('click', (e) => {
+    if(e.target === popupBg4) {
+        popupBg4.classList.remove('active');
+        popup4.classList.remove('active');
+    }
+});
+
+let popupBg5 = document.querySelector('.popup__bg5');
+let popup5 = document.querySelector('.popup5');
+let openPopupButtons5 = document.querySelectorAll('.open-popup5');
+let closePopupButton5 = document.querySelector('.close-popup5');
+
+openPopupButtons5.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        e.preventDefault();
+        popupBg5.classList.add('active');
+        popup5.classList.add('active');
+    })
+});
+
+closePopupButton5.addEventListener('click',() => {
+    popupBg5.classList.remove('active');
+    popup5.classList.remove('active');
+});
+
+document.addEventListener('click', (e) => {
+    if(e.target === popupBg5) {
+        popupBg5.classList.remove('active');
+        popup5.classList.remove('active');
+    }
+});
